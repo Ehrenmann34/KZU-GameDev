9c0d65b4f89e4d4e2e7eea4f3403d868c8cd9ba2
Ehrenmann34 <mattia.erne@gmx.ch>
2023-05-15 16:06:41 +0200

    Bugs fixxed

--- KZU-GameDev/Assets/Scripts/CameraFollow.cs
+++ KZU-GameDev/Assets/Scripts/CameraFollow.cs
@@ -14,7 +14,7 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
