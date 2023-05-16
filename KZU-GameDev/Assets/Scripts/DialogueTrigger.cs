b167c0372d25bb43f8037127989d72660bbc1a85
Ehrenmann34 <mattia.erne@gmx.ch>
2023-05-16 11:57:49 +0200

    Dialogue Trigger done

--- KZU-GameDev/Assets/Scripts/DialogueTrigger.cs
+++ KZU-GameDev/Assets/Scripts/DialogueTrigger.cs
@@ -4,13 +4,67 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Message[] messages;
-    public Actor[] actors;
+    public GameObject rose;
+    public GameObject slot1;
+
+    public static bool roseQuestGoing = false;
+    public static bool roseQuestFinished = false;
+
+    public Message[] messages1;
+    public Actor[] actors1;
+
+    public Message[] messages2;
+    public Actor[] actors2;
+
+    public Message[] messages3;
+    public Actor[] actors3;
+
+    public Message[] messages4;
+    public Actor[] actors4;
 
     public void StartDialogue()
     {
-        FindObjectOfType<DialogueManager>().OpenDialogue(messages, actors);
+
+        if(roseQuestFinished == false && roseQuestGoing == false)
+        {
+            FindObjectOfType<DialogueManager>().OpenDialogue(messages1, actors1);
+            // Rose aktivieren
+            // QuestOnGoing true
+        }
+
+        if(rose != null && roseQuestGoing == true)
+        {
+            FindObjectOfType<DialogueManager>().OpenDialogue(messages2, actors2);
+        }
+
+        if(rose == null && roseQuestGoing == true && slot1.transform.childCount > 0)
+        {
+            FindObjectOfType<DialogueManager>().OpenDialogue(messages3, actors3);
+            // Quest finished true
+            // Quest onGoing false
+        }
+
+        if(rose == null && roseQuestFinished == true)
+        {
+            FindObjectOfType<DialogueManager>().OpenDialogue(messages4, actors4);    
+        }
     }
+
+    void Update()
+    {
+        if(roseQuestFinished == false && roseQuestGoing == false && DialogueManager.isActive == true)
+        {
+            rose.SetActive(true);
+            roseQuestGoing = true;
+        }
+        if(rose == null && roseQuestGoing == true && slot1.transform.childCount > 0 && DialogueManager.isActive == true)
+        {
+            roseQuestFinished = true;
+            roseQuestGoing = false;
+            GameObject.Destroy(slot1.transform.GetChild(0).gameObject);
+        }
+    }
+
 }
 
 [System.Serializable]
